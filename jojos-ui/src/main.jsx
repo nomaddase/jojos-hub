8adf538fec34275901f4416c1ce5e132ff74ab41
nomaddase <67481591+nomaddase@users.noreply.github.com>
2026-08-09 12:07:27 +0500

    display: load compact 1920x1080 order-board overrides

--- jojos-ui/src/main.jsx
+++ jojos-ui/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 import './contrast.css'
+import './display-compact.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
